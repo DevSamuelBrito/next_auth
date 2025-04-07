e4e994a0f9ab3d3af3eddc250fa08c7041939099
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-07 17:18:04 -0300

    adiciona rota GET para recuperar informações do usuário autenticado

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "User not found" }, { status: 401 });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user?.email as string,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+    },
+  });
+
+  if(!user ){
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(user, { status: 200 });
+}
