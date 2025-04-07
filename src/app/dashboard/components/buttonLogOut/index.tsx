15f8bc2cd3527490ba1b1bdfc0817dd0123cca4d
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-07 17:17:24 -0300

    adiciona mensagem de console ao deslogar e define callbackUrl na função signOut

--- src/app/dashboard/components/buttonLogOut/index.tsx
+++ src/app/dashboard/components/buttonLogOut/index.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { signOut } from "next-auth/react";
 export function ButtonLogOut() {
+
+
     return (
         <button
             className="bg-red-100 border-2 rounded-2xl font-bold px-2 py-2 w-20"
-            onClick={()=> signOut()}
+            onClick={()=>{
+                signOut({ callbackUrl: '/' },
+                )
+                console.log("Usuário deslogado"); 
+            }}
         >Sair</button>
     );
 }
