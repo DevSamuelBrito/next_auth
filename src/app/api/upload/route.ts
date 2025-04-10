57deb226f315c6acc3e380ba6840647664aaee45
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-10 15:31:56 -0300

    enviando o link das imagens para o banco

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -8,6 +11,12 @@ cloudinary.config({
 });
 
 export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "User not authorized" }, { status: 401 });
+  }
+
   try {
     const formData = await req.formData();
     const file = formData.get("file") as Blob | null;
@@ -19,13 +28,31 @@ export async function POST(req: Request) {
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
-    const uploadReponse = await new Promise((resolve, reject) => {
+    const uploadReponse = await new Promise<any>((resolve, reject) => {
       cloudinary.uploader
         .upload_stream({ folder: "images" }, (error, result) => {
           if (error) reject(error);
           resolve(result);
         })
         .end(buffer);
+    });
+
+    if (!uploadReponse.secure_url) {
+      return NextResponse.json(
+        { error: "Error uploading Image" },
+        { status: 500 }
+      );
+    }
+
+    await prisma.user.update({
+      where: {
+        email: session.user?.email as string,
+      },
+      data: {
+        images: {
+          push: uploadReponse.secure_url,
+        },
+      },
     });
 
     return NextResponse.json(uploadReponse);
