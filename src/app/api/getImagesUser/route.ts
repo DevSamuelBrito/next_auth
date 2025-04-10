55302cb42edfc30fd6310a364d5f56525a4228e9
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-10 16:20:10 -0300

    implementa endpoint GET para recuperar imagens do usuário autenticado

---
+++ src/app/api/getImagesUser/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user?.email as string,
+    },
+    select: { images: true },
+  });
+
+  return NextResponse.json({ images: user?.images || [] });
+}
