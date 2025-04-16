0f316d7e59f5fbdcb95fff7ce71b3b2e9f7ef51c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-16 14:48:27 -0300

    instalando e configurando o Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Next Auth",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider attribute="class">
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
