78cdf06ce6a961fc0b274bdc23c114568ce48073
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-04-27 21:44:40 -0300

    criando div padrao para a pagina de profile

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,7 +1,9 @@
 const Profile = () => {
     return (
-        <div>
-            Pg do Profile
+        <div className="space-y-4 flex flex-col items-center justify-center 2xl:mx-24 mx-15 bg-[#171717] rounded-2xl mb-4">
+            <p>
+                teste
+            </p>
         </div>
     );
 }
