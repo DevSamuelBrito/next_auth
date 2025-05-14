7d9a6e2aa38f47aa1cdb7b9fe4c403b2b270d9ef
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-05-14 16:43:24 -0300

    adicionando componente ChangeImageCard para alterar imagem de perfil

---
+++ src/app/(changeProfile)/changepicture/components/ChangeImageCard/index.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { redirect } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { ArrowLeft } from "lucide-react"
+import PhotoProfile from "@/app/dashboard/profile/components/PhotoProfile";
+
+export function ChangeImageCard({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+
+    const [imageUrl, setImageUrl] = useState<string>(""); 
+
+    const fetchImage = async () => {
+        const res = await fetch("/api/user/profile-image",
+            {
+                method: "GET",
+                cache: "no-store",
+            }
+        );
+        const data = await res.json();
+        console.log(data);
+        setImageUrl(data.profilePicture);
+    }
+
+    useEffect(()=>{
+        fetchImage();
+    },[])
+
+  
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <ArrowLeft size={20} className="mb-2" onClick={() => redirect("/dashboard/profile")} cursor="pointer" />
+                    <CardTitle>Alterar Imagem de Perfil:</CardTitle>
+                    <CardDescription>
+                        Seleciona uma imagem para o seu perfil.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                        <div className="flex flex-col gap-6">
+                            <div className="flex items-center justify-center">
+                                <PhotoProfile image={imageUrl}/>
+                            </div>
+                        </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
--- src/app/dashboard/profile/components/PhotoProfile/index.tsx
+++ src/app/dashboard/profile/components/PhotoProfile/index.tsx
@@ -1,44 +1,63 @@
 "use client";
 import { User } from "lucide-react";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 interface PhotoProfiileProps {
     image?: string | null;
 }
 
 const PhotoProfile = ({ image }: PhotoProfiileProps) => {
-    const inputRef = useRef<HTMLInputElement>(null)
-    const [preview, setPreview] = useState<string | null>(image || null)
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [preview, setPreview] = useState<string | null>(image || null);
+
+    useEffect(() => {
+        if (image) {
+            setPreview(image);
+        }
+    }, [image]);
 
     const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
-
         if (!file) return;
-        const formData = new FormData();
 
-        formData.append("file", file)
+        const formData = new FormData();
+        formData.append("file", file);
 
         const res = await fetch("/api/user/photo", {
             method: "POST",
             body: formData,
-        })
+        });
+
         const data = await res.json();
 
         if (res.ok) {
             toast.success("Imagem enviada com sucesso");
-            setPreview(data.imageUrl)
+            setPreview(data.imageUrl);
+        } else {
+            toast.error("Erro ao enviar a imagem");
+            setPreview(null);
+        }
+    };
+
+    const handleDelete = async () => {
+        const res = await fetch("/api/user/deletepictureprofile", {
+            method: "DELETE",
+        });
+        if (res.ok) {
+            toast.success("Imagem excluída com sucesso");
+            setPreview(null);
         }
         else {
-            toast.error("Error ao enviar a imagem");
-            setPreview(null)
+            toast.error("Erro ao excluir a imagem");
         }
-
     }
 
     return (
-        <div>
+        <div className="w-full flex flex-col items-center gap-4">
             <input
                 type="file"
                 onChange={handleChange}
@@ -47,24 +66,54 @@ const PhotoProfile = ({ image }: PhotoProfiileProps) => {
                 className="hidden"
             />
             <div
-                className="w-24 h-24 rounded-full overflow-hidden bg-black flex items-center justify-center"
-                onClick={() => inputRef.current?.click()}
+                className="w-52 h-52 rounded-full overflow-hidden bg-black flex items-center justify-center"
+
             >
                 {preview ? (
                     <Image
                         src={preview}
                         alt="User"
-                        width={124}
-                        height={124}
+                        width={208}
+                        height={208}
                         className="w-full h-full object-cover"
                     />
                 ) : (
-                    <User size={30} className="text-white" />
+                    <User size={120} className="text-white" />
                 )}
             </div>
 
+            <div className="flex gap-4">
+                <Button type="button" className="cursor-pointer" onClick={() => inputRef.current?.click()}>
+                    Adicionar Imagem
+                </Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="destructive" disabled={!preview} className="cursor-pointer" >Excluir Imagem</Button>
+                    </DialogTrigger>
+
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Tem certeza que deseja excluir?</DialogTitle>
+                            <DialogDescription>
+                                Essa imagem será excluída permanentemente e não poderá ser recuperada.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button variant="destructive" onClick={handleDelete} className="cursor-pointer">
+                                    Confirmar
+                                </Button>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Button variant="outline" className="cursor-pointer">Cancelar</Button>
+                            </DialogClose>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+
+            </div>
         </div>
     );
-}
+};
 
 export default PhotoProfile;
