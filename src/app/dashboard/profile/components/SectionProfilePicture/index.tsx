984cc9611cc473386c970345fcfa7852d7b3cc9c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-05-14 16:42:39 -0300

    removendo a opcao de alterar foto pela pagina profile

--- src/app/dashboard/profile/components/SectionProfilePicture/index.tsx
+++ src/app/dashboard/profile/components/SectionProfilePicture/index.tsx
@@ -1,6 +1,10 @@
+"use client";
 import { SimpleUser } from "@/types/user";
 import PhotoProfile from "../PhotoProfile";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import { User } from "lucide-react";
 
 interface SectionProfilePictureProps {
     user: SimpleUser;
@@ -12,10 +16,28 @@ const SectionProfilePicture = ({ user }: SectionProfilePictureProps) => {
             <div className="flex items center justify-between w-full p-6">
                 <div className="flex justify-center items-center gap-x-36">
                     <span className="text-[#96938d] text-sm font-bold w-28">Foto de perfil</span>
-                    <PhotoProfile image={user?.profilePicture} />
+                    {
+                        user?.profilePicture ? (
+                            <Image
+                                src={user?.profilePicture}
+                                alt="User"
+                                width={124}
+                                height={124}
+                                className="w-24 h-24 rounded-full object-cover"
+                            />
+                        ) : (
+                             <Image
+                                src={"/avatar.svg"}
+                                alt="User"
+                                width={124}
+                                height={124}
+                                className="w-24 h-24 rounded-full object-cover"
+                            />
+                        )
+                    }
                 </div>
                 <div className="flex items-center justify-center">
-                    <Button className="cursor-pointer">
+                    <Button className="cursor-pointer" onClick={() => { redirect("/changepicture") }}>
                         Alterar
                     </Button>
                 </div>
