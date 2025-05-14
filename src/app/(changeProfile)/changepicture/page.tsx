a2018fd3709096167522dfd02eb850b70b0d4794
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-05-14 16:40:18 -0300

    criando pagina de alteracaode foto

---
+++ src/app/(changeProfile)/changepicture/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ChangeImageCard } from "./components/ChangeImageCard";
+
+
+const ChangePassword = () => {
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <ChangeImageCard />
+            </div>
+        </div>
+    );
+}
+
+export default ChangePassword;
