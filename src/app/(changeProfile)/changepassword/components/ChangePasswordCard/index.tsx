620bda5dd21506232ba337e3b786c69c2b9889d6
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-05-14 11:49:27 -0300

    colocando a api de alterar senha na nova pagina

--- src/app/(changeProfile)/changepassword/components/ChangePasswordCard/index.tsx
+++ src/app/(changeProfile)/changepassword/components/ChangePasswordCard/index.tsx
@@ -12,41 +12,49 @@ import { Label } from "@/components/ui/label"
 import { redirect } from "next/navigation";
 import React, { useState } from "react";
 import { ArrowLeft } from "lucide-react"
+import { toast } from "sonner"
 
 export function ChangePasswordCard({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("") //senha atual
+  const [newPassword, setNewPassword] = useState(""); // nova senha
+  const [confirmPassword, setConfirmPassword] = useState(""); // confirmar nova senhaF
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const response = await fetch("/api/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ name, email, password })
-    })
-
-    const data = await response.json()
-
-    if (!response.ok) {
-      alert(data.error || "Erro ao criar usuário")
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      toast.error("Novas Senhas não coincidem");
       return;
     }
 
-    alert("Usuário criado com sucesso!");
-    redirect("/");
+    const res = await fetch("/api/change-password", {
+      method: "POST",
+      body: JSON.stringify({
+        currentPassword,
+        newPassword,
+      })
+    });
+
+
+    if (!res.ok) {
+      toast.error("Erro ao alterar senha");
+      return;
+    }
+
+    toast.success("Senha alterada com sucesso");
+    setCurrentPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+    redirect("/dashboard/profile");
   }
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <ArrowLeft size={20} className="mb-2"  onClick={()=>redirect("/dashboard/profile")} cursor="pointer"/>
+          <ArrowLeft size={20} className="mb-2" onClick={() => redirect("/dashboard/profile")} cursor="pointer" />
           <CardTitle>Alterar Senha</CardTitle>
           <CardDescription>
             Preencha os campos abaixo para alterar sua senha.
@@ -60,11 +68,10 @@ export function ChangePasswordCard({
                 <Input
                   id="name"
                   type="password"
-                  value={name}
+                  value={currentPassword}
                   placeholder="********"
-
                   required
-                  onChange={(e) => setName(e.target.value)}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
@@ -74,9 +81,9 @@ export function ChangePasswordCard({
                   type="password"
                   placeholder="********"
 
-                  value={email}
+                  value={newPassword}
                   required
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setNewPassword(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
@@ -85,10 +92,10 @@ export function ChangePasswordCard({
                 </div>
                 <Input
                   id="password"
-                  value={password}
+                  value={confirmPassword}
                   type="password"
                   required
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                   placeholder="********"
                 />
               </div>
