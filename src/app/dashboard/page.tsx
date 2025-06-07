b1ff138429abd75431ec0faf753e7575561a768d
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-06 23:22:08 -0300

    refatorando a pagina de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,10 @@
 import { authOptions } from "@/lib/auth"
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth"
-import Image from "next/image"
 import { redirect } from "next/navigation";
+import PublicImagesList from "./components/PublicImageList";
 
-
-
-export default async function Page() {
+export default async function Dashboard() {
 
     const session = await getServerSession(authOptions);
 
@@ -22,33 +20,10 @@ export default async function Page() {
     return (
 
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <p>All Photos</p>
-            {/* <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" /> */}
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3 w-full">
-                {
-                    publicImages.length > 0 ? (
-
-                        publicImages.map((image, index) => (
-                            image ? (
-
-                                <div key={index} className="aspect-video rounded-xl">
-                                    <Image
-                                        src={image.secureUrl}
-                                        alt={`Image ${index}`}
-                                        className="w-full h-full object-cover rounded-xl"
-                                        width={300}
-                                        height={200}
-                                    />
-                                </div>
-                            ) : null
-                        ))
-                    ) : (
-                        <div className="flex items-center justify-center col-span-full row-span-full min-h-[400px]">
-                            <p className="text-center text-gray-200">Nenhuma Imagem Encontrada</p>
-                        </div>
-                    )}
-            </div>
+            <p>Imagens Públicadas:</p>
+            <PublicImagesList images={publicImages} />
         </div>
+
     )
 }
 
