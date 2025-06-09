0b73187029094ae0f87b871eab1f4eae80c653c1
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-08 23:05:06 -0300

    refatorando a função de excluir  imagem no componente CardContainer

--- src/app/dashboard/account/components/ImageUpload/components/CardContainer/index.tsx
+++ src/app/dashboard/account/components/ImageUpload/components/CardContainer/index.tsx
@@ -16,36 +16,35 @@ interface CardContainerProps {
 
 
 export default function CardContainer({ img, setImageUrl }: CardContainerProps) {
+
+    const DeleteImage = async () => {
+        const res = await fetch("/api/deleteImage", {
+            method: "DELETE",
+            body: JSON.stringify({ publicId: img.publicId }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (res.ok) {
+            toast.success("Imagem excluída com sucesso!");
+
+            setImageUrl((prev) => prev.filter((image) => image.publicId !== img.publicId));
+        } else {
+            toast.error("Erro ao excluir imagem");
+        }
+    }
+
     return (
-        <div className="bg-[#292828] rounded-2xl flex flex-col items-center justify-center" key={img.id}>
+        <div className="bg-[#292828] rounded-2xl border-[1px] border-gray flex flex-col items-center justify-center" key={img.id}>
             <CardImagem
                 key={img.id}
                 img={img}
-                onDelete={
-                    async () => {
-                        const res = await fetch("/api/deleteImage", {
-                            method: "DELETE",
-                            body: JSON.stringify({ publicId: img.publicId }),
-                            headers: { "Content-Type": "application/json" },
-                        });
-                        if (res.ok) {
-                            toast.success("Imagem excluída com sucesso!");
-
-                            setImageUrl((prev) => prev.filter((image) => image.publicId !== img.publicId));
-                        } else {
-                            toast.error("Erro ao excluir imagem");
-                        }
-                    }
-                }
+                onDelete={DeleteImage}
             />
             <div className="flex flex-col items-center w-[200px] justify-center  overflow-hidden">
                 <p className="font-bold  overflow-hidden whitespace-nowrap text-ellipsis w-full text-center">{img.name}</p>
                 <p className="italic overflow-hidden whitespace-nowrap text-ellipsis w-full text-center">
                     {img.description}
                 </p>
-                
             </div>
-
         </div>
     )
 }
