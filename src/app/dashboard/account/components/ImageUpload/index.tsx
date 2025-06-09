13c1770c689d7efb3928142cb8b3c1d2684a74d5
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-09 17:10:17 -0300

    refatorando: implementando o componente de form

--- src/app/dashboard/account/components/ImageUpload/index.tsx
+++ src/app/dashboard/account/components/ImageUpload/index.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { toast } from "sonner";
 import { getSession } from "next-auth/react";
-import { Input } from "@/components/ui/input"
 import { ImageData } from "@/types/imageData";
-import CardContainer from "./components/CardContainer";
-import { Switch } from "@/components/ui/switch";
-import { Label } from "@/components/ui/label";
 import UserGallery from "./components/UserGallery";
+import FormUploadImage from "./components/FormUploadImage";
 
 
 export default function ImageUpload() {
@@ -118,104 +112,37 @@ export default function ImageUpload() {
         }
     }
 
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.target.files?.[0] || null;
+        setFile(selectedFile);
+        setUploadError(false);
+
+        if (selectedFile) {
+            const objectUrl = URL.createObjectURL(selectedFile);
+            setPreview(objectUrl);
+        } else {
+            setPreview(null);
+        }
+    };
+
+
     return (
         <div>
-            <form
-                className="space-y-4 flex flex-col items-center justify-center 2xl:mx-24 mx-15 bg-[#171717] rounded-2xl mb-4"
-                onSubmit={handleUpload}
-            >
-                <label
-                    htmlFor="imageInput"
-                    className="text-bold text-[#96938d] hover:text-white cursor-pointer mt-4"
-                >
-                    Selecionar Imagem
-                </label>
-
-                <input
-                    id="imageInput"
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                        const selectedFile = e.target.files?.[0] || null;
-                        setFile(selectedFile);
-                        setUploadError(false);
-
-                        if (selectedFile) {
-                            const objectUrl = URL.createObjectURL(selectedFile);
-                            setPreview(objectUrl);
-                        } else {
-                            setPreview(null);
-                        }
-                    }}
-                    className="hidden"
-                />
-
-                {preview && (
-
-                    <div className="flex flex-col p-5 items-center justify-center w-full ">
-                        <div className="mt-4 flex flex-col items-center w-1/2  ">
-                            <p className="text-base text-gray-400 mb-2">Preview:</p>
-                            <Image
-                                src={preview}
-                                alt="Preview da imagem selecionada"
-                                width={300}
-                                height={200}
-                                className="rounded-xl object-cover max-w-32 max-h-32 lg:max-h-64 lg:max-w-64"
-                            />
-                            <button
-                                type="button"
-                                onClick={() => {
-                                    setFile(null);
-                                    setPreview(null);
-                                }}
-                                className="mt-2 text-red-500 hover:underline text-sm"
-                            >
-                                Remover Imagem
-                            </button>
-
-                        </div>
-                        <div className="mt-4 flex flex-col w-1/2 gap-3 items-center">
-                            <Input
-                                id="name"
-                                type="text"
-                                value={nameImage}
-                                placeholder="Nome da Imagem"
-                                required
-                                disabled={loading}
-                                onChange={(e) => { setNameImage(e.target.value) }}
-                            />
-                            <Textarea
-                                id="description"
-                                value={descriptionImage}
-                                placeholder="Descrição da Imagem"
-                                className="h-24 w-full text-start align-top py-2"
-                                disabled={loading}
-                                required
-                                onChange={(e) => { setDescriptionImage(e.target.value) }}
-                            />
-                            <div className="flex items-center gap-2">
-                                <Switch
-                                    id="Privado"
-                                    checked={privateImage}
-                                    onCheckedChange={(checked) => {
-                                        setPrivateImage(checked);
-                                    }}
-                                />
-                                <Label htmlFor="Privado">{privateImage ? "Imagem Privada" : "Imagem Publica"}</Label>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
-                <Button variant="outline" className="mt-4 mb-4 w-52" type="submit" disabled={loading}>
-                    {loading ? "Enviando..." : "Enviar Imagem"}
-                </Button>
-                {
-                    uploadError && (
-                        <p className="mb-2 text-red-500">Você precisa selecionar uma Imagem para fazer Upload!</p>
-                    )
-                }
-            </form>
+            <FormUploadImage 
+            onFileChange={handleFileChange}
+            onFileUpload={handleUpload}
+            preview={preview}
+            setPreview={setPreview}
+            loading={loading}
+            uploadError={uploadError}
+            setFile={setFile}
+            nameImage={nameImage}
+            setNameImage={setNameImage}
+            descriptionImage={descriptionImage}
+            setDescriptionImage={setDescriptionImage}
+            privateImage={privateImage}
+            setPrivateImage={setPrivateImage}
+            />
 
             <UserGallery imageUrl={imageUrl} session={session} setImageUrl={setImageUrl} />
         </div>
