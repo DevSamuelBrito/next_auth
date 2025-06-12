c9784f751a4de1cc0057edb59ba3543d28b1eca4
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-12 09:39:07 -0300

    refatorando: substituindo a logica de exibir imagens pela utilizacao do componente CardImageDashboard

--- src/app/dashboard/components/PublicImageList/index.tsx
+++ src/app/dashboard/components/PublicImageList/index.tsx
@@ -2,8 +2,9 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { CardImageDashBoard } from "./components/CardImageDashboard";
 
-interface PublicImage {
+export interface PublicImage {
     id: string;
     secureUrl: string;
     publicId: string;
@@ -17,9 +18,6 @@ interface PublicImage {
 const PublicImagesList = ({ images }: { images: PublicImage[] }) => {
     const [selectedImage, setSelectedImage] = useState<PublicImage | null>(null);
 
-
-
-
     if (!images.length) {
         return <div className="flex items-center justify-center col-span-full row-span-full min-h-[400px]">
             <p className="text-center text-gray-200">Nenhuma Imagem Encontrada</p>
@@ -32,15 +30,8 @@ const PublicImagesList = ({ images }: { images: PublicImage[] }) => {
             <div className="grid auto-rows-min gap-4 md:grid-cols-3 w-full">
                 {
                     images.map((image, index) => (
-                        <div key={index} className="bg-[#292828] rounded-xl border-[1px] border-gray flex flex-col items-center justify-center aspect-video ">
-                            <Image
-                                onClick={() => setSelectedImage(image)}
-                                src={image.secureUrl}
-                                alt={`Image ${index}`}
-                                className="w-full h-full object-cover rounded-xl"
-                                width={300}
-                                height={200}
-                            />
+                        <div className="bg-[#292828] rounded-2xl border-[1px] border-gray flex flex-col items-center justify-center" key={image.id}>
+                            <CardImageDashBoard key={index} img={image} />
                             <div className="flex flex-col items-center w-[200px] justify-center  overflow-hidden">
                                 <p className="font-bold  overflow-hidden whitespace-nowrap text-ellipsis w-full text-center">{image.name}</p>
                                 <p className="italic overflow-hidden whitespace-nowrap text-ellipsis w-full text-center">
@@ -51,36 +42,7 @@ const PublicImagesList = ({ images }: { images: PublicImage[] }) => {
                     ))
                 }
             </div>
-            {selectedImage && (
-                <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-white rounded-lg p-6 w-[90%] max-w-md shadow-lg relative">
-                        <button
-                            onClick={() => setSelectedImage(null)}
-                            className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-                        >
-                            ✕
-                        </button>
-                        <Image
-                            src={selectedImage.secureUrl}
-                            alt={selectedImage.name}
-                            width={500}
-                            height={300}
-                            className="rounded-md mb-4"
-                        />
-                        <h2 className="text-lg font-semibold mb-2">{selectedImage.name}</h2>
-                        <p className="text-sm text-gray-600 mb-4">{selectedImage.description}</p>
-                        <button
-                            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-                            onClick={() => {
-                                // Aqui você chama uma função para salvar nos favoritos
-                                console.log("Salvar imagem:", selectedImage.id);
-                            }}
-                        >
-                            Salvar nos Favoritos
-                        </button>
-                    </div>
-                </div>
-            )}
+
         </>
 
     )
