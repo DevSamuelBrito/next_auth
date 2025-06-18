b68c215cc5bd3deb025e621ecb6bbfabbe30ca03
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-18 15:48:00 -0300

    refatorando o searchInput para recber os parametros de busca

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -5,50 +5,38 @@ import { LoaderCircleIcon, SearchIcon } from "lucide-react"
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Button } from "./ui/button"
 
-export default function SearchInput() {
-  const id = useId()
-  const [inputValue, setInputValue] = useState("")
-  const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    if (inputValue) {
-      setIsLoading(true)
-      const timer = setTimeout(() => {
-        setIsLoading(false)
-      }, 500)
-      return () => clearTimeout(timer)
-    }
-    setIsLoading(false)
-  }, [inputValue])
+interface SearchInputProps {
+  onSearch: () => void;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function SearchInput({ onSearch, value, onChange }: SearchInputProps) {
 
   return (
-    <div className="*:not-first:mt-2">
-      <Label htmlFor={id}>Search input with loader</Label>
+    <div className="*:not-first:mt-2 ">
       <div className="relative">
         <Input
-          id={id}
-          className="peer ps-3 pe-9"
+          className="peer ps-3 pe-9 w-full"
           placeholder="Search..."
           type="search"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          value={value}
+          onChange={onChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              onSearch();
+            }
+          }
+          }
         />
         <button
           type="button"
           className="absolute right-3 top-1/2 -translate-y-1/2 p-0 m-0 h-4 w-4 text-muted-foreground/80 flex items-center justify-center cursor-pointer"
+          onClick={onSearch}
         >
-          {isLoading ? (
-            <LoaderCircleIcon
-              className="animate-spin"
-              size={16}
-              role="status"
-              aria-label="Loading..."
-            />
-          ) : (
-            <SearchIcon size={16} aria-hidden="true" />
-          )}
+          <SearchIcon size={16} aria-hidden="true" />
         </button>
 
       </div>
