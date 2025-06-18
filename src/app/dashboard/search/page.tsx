b1280ca4e34e8aa2b59175abf0e58ac05ff83aab
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-18 15:48:18 -0300

    implementando a busca e melhorando a estrutura do componente de pesquisa

--- src/app/dashboard/search/page.tsx
+++ src/app/dashboard/search/page.tsx
@@ -1,29 +1,62 @@
 "use client"
 import SearchInput from "@/components/search-input";
 import { useState } from "react";
-import { PublicImage } from "../components/PublicImageList";
+import PublicImagesList, { PublicImage } from "../components/PublicImageList";
 import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 
 const Search = () => {
-    const [seach,setSeatch] = useState("");
+    const [search, setSearch] = useState("");
     const [images, setImages] = useState<PublicImage[]>([]);
+
+    const handleClear = () => {
+        setSearch("");
+        setImages([]);
+        toast.success("Pesquisa limpa");
+    }
 
     const handleSearch = async () => {
         try {
-            
-        } catch (error) {  
+            const response = await fetch(`/api/search?query=${search}`)
+            const data = await response.json()
+            setImages(data.images);
+            if (data.images.length === 0) {
+                toast.error("Nenhuma imagem encontrada");
+            } else {
+                toast.success("Imagens encontradas com sucesso");
+            }
+        } catch (error) {
             toast.error("Erro ao pesquisar Imagem");
         }
     }
 
     return (
         <>
-            <div className="w-full flex items-center bg-red-900 justify-center">
-                <div className="w-full max-w-xl">
-                {/* <SearchInput  onSearch={handleSearch}/> */}
+            <div className="w-full min-h-screen bg-neutral-950 px-4 py-8">
+                <div className="max-w-5xl mx-auto flex flex-col gap-8">
+                    <h1 className="text-2xl font-bold">Buscar por imagens...</h1>
+                    <div className="flex items-center">
+                        <div className="flex-1">
+                            <SearchInput
+                                onSearch={handleSearch}
+                                onChange={(e) => setSearch(e.target.value)}
+                                value={search}
+                            />
+                        </div>
+                        <Button onClick={handleClear} className="ml-4">
+                            Limpar
+                        </Button>
+                    </div>
+
+
+                    <div>
+                        <PublicImagesList images={images} />
+                    </div>
                 </div>
             </div>
+
         </>
     );
 }
