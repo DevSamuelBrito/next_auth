9553d32652215a77cf744734f77ef04994d2a1d0
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-19 10:47:44 -0300

    criando pagina home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/hero";
+import NavbarHome from "@/components/nav-home";
+
+const Home = () => {
+    return (
+        <div>
+            <NavbarHome />
+            <HeroSection />
+        </div>
+    );
+}
+
+export default Home;
