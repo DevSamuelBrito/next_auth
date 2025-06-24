58ba1669236066da89fafe877bdafa0709a1a28c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-24 11:34:27 -0300

    adicionando componentes StatsSection, ContentTwo, FAQ e FooterSection a home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,10 @@
 import ContentSection from "@/components/content-hero";
 import HeroSection from "@/components/hero";
 import NavbarHome from "@/components/nav-home";
+import ContentTwo from "../../components/content-two";
+import StatsSection from "@/components/stats";
+import FAQ from "@/components/faq";
+import FooterSection from "@/components/footer";
 
 const Home = () => {
     return (
@@ -8,6 +12,10 @@ const Home = () => {
             <NavbarHome />
             <HeroSection />
             <ContentSection />
+            <StatsSection />
+            <ContentTwo />
+            <FAQ />
+            <FooterSection />
         </div>
     );
 }
