c21eedd345577c0c4477c8414f8c2c03496a85cc
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-26 16:58:30 -0300

    adicionando componente ScrollToTopButton para rolagem suave ao topo da página

---
+++ src/components/scroll-to-top.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useState, useEffect } from "react";
+
+const ScrollToTopButton = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 750) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    return (
+        isVisible && (
+            <button
+                onClick={scrollToTop}
+                className="fixed bottom-4 right-4 p-4 bg-gray-500 text-white rounded-full shadow-lg hover:bg-gray-600 transition-all duration-700 w-12 h-12 flex items-center justify-center"
+            >
+                <span className="text-2xl">↑</span>
+            </button>
+
+        )
+    );
+};
+
+export default ScrollToTopButton;
