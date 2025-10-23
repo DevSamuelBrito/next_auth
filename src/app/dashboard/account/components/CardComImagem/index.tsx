2c079f89c410503def45af486cbf6321f99e26b9
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-10-23 15:12:36 -0300

    style: altera grid de 3 para 4 em telas maiores, e concerta imagens vazandos

--- src/app/dashboard/account/components/CardComImagem/index.tsx
+++ src/app/dashboard/account/components/CardComImagem/index.tsx
@@ -89,14 +89,16 @@ export function CardImagem({ img, onDelete }: { img: any, onDelete: (id: string)
                     <DialogTitle className="sr-only">Visualizar imagem</DialogTitle>
                 </DialogHeader>
                 {/* Dialog para PREVIEW */}
-                <DialogTrigger asChild className=" z-10 aspect-video rounded-xl  mt-1 bg-blackoverflow-hidden">
-                    <Image
-                        src={img.secureUrl}
-                        alt={`Image ${img.id}`}
-                        className="w-full h-full object-contain rounded-xl cursor-pointer transition duration-200"
-                        width={300}
-                        height={200}
-                    />
+                <DialogTrigger asChild className="z-10 mt-1">
+                    <div className="relative aspect-video rounded-xl bg-black overflow-hidden">
+                        <Image
+                            src={img.secureUrl}
+                            alt={`Image ${img.id}`}
+                            className="object-contain rounded-xl cursor-pointer transition duration-200"
+                            fill
+                            sizes="(max-width: 640px) 100vw, 33vw"
+                        />
+                    </div>
                 </DialogTrigger>
                 {/*Dialog quando aberto o Modal*/}
                 <DialogContent
@@ -158,6 +160,7 @@ export function CardImagem({ img, onDelete }: { img: any, onDelete: (id: string)
             <Dialog>
                 <DialogTrigger asChild>
                     <button
+                    aria-label="Botão de deletar"
                         className="z-1 absolute top-2 right-2 bg-red-500 text-white p-2 rounded-full opacity-0 group-hover:opacity-100 transition border-2 border-white hover:scale-110 duration-150"
                     >
                         <Trash className="text-white" />
--- src/app/dashboard/account/components/ImageUpload/components/UserGallery/index.tsx
+++ src/app/dashboard/account/components/ImageUpload/components/UserGallery/index.tsx
@@ -34,7 +34,7 @@ const UserGallery = ({ imageUrl, session, setImageUrl, loadingImage }: UserGalle
         );
     } else {
         content = (
-            <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+            <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 items-center justify-center w-11/12">
                 {imageUrl.map((img) => (
                     <CardContainer img={img} setImageUrl={setImageUrl} key={img.id} />
                 ))}
